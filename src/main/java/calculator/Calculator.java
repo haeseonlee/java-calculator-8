4537045a9dd7b28d3a963f1a30d33247e0c166bc
haeseon lee <haeseon4816@gmail.com>
2025-10-16 10:13:54 +0900

    숫자 덧셈 기능 구현

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import java.util.List;
+
+public class Calculator {
+
+    public int calculate(List<Integer> numbers) {
+        return numbers.stream().mapToInt(n -> n).sum();
+    }
+}
