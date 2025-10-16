c85fc4397a2cef2a37cc8a1fe5a5c9a68b685bae
haeseon lee <haeseon4816@gmail.com>
2025-10-16 14:30:44 +0900

    문자열 숫자 리스트 변환 메서드에 예외 처리 로직 추가

--- src/main/java/calculator/StringToNumberConverter.java
+++ src/main/java/calculator/StringToNumberConverter.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class StringToNumberConverter {
 
     public static List<Integer> convert(List<String> values) {
-        return values.stream().map(Integer::parseInt).toList();
+        try {
+            return values.stream().map(Integer::parseInt).toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력값입니다.");
+        }
     }
 }
