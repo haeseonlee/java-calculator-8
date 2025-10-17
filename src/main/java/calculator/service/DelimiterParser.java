8d9d7c31d13e5838dd5856d9405bd55e153b7337
haeseon lee <haeseon4816@gmail.com>
2025-10-17 10:14:35 +0900

    메타문자 포함 문자열 이스케이프 처리

--- src/main/java/calculator/service/DelimiterParser.java
+++ src/main/java/calculator/service/DelimiterParser.java
@@ -2,6 +2,7 @@ package calculator.service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class DelimiterParser {
 
@@ -20,7 +21,10 @@ public class DelimiterParser {
     private List<String> customParser(String input) {
         String replacedStr = input.replaceAll("//", "").replace("\\n", "");
         String delimiter = replacedStr.substring(0, 1);
-        return Arrays.stream(replacedStr.substring(1).split(delimiter))
+
+        String safeDelimiter = Pattern.quote(delimiter);
+
+        return Arrays.stream(replacedStr.substring(1).split(safeDelimiter))
                 .toList();
     }
 
